993a279d3f0a7b443c4e5408d75b3548ba113b01
CodigoSincero <codeasgamaer@gmail.com>
2021-10-13 21:37:09 -0300

    scroll down facebook

--- pupScript.js
+++ pupScript.js
@@ -7,7 +7,7 @@ async function scrape(Itens, getdata, personalfunction = () => {}) {
 
   for (let item of Itens) {
     let confDebug = { headless: false }
-    const browser = await puppeteer.launch(confDebug);
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setUserAgent(
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
